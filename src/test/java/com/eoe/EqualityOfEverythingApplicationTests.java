1f8b3029d6eacc1903085ad4c2642edabd27cf4b
Gibran <3470380398@qq.com>
2023-11-27 16:52:15 +0800

    init

---
+++ src/test/java/com/eoe/EqualityOfEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualityOfEverythingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
