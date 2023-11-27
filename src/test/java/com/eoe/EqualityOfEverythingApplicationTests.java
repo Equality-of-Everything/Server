8cbc0deadf9bc5f3c67ea7ed6b4641dbdc6c64b2
Gibran <3470380398@qq.com>
2023-11-27 16:52:15 +0800

    合并

--- src/test/java/com/eoe/EqualityOfEverythingApplicationTests.java
+++ src/test/java/com/eoe/EqualityOfEverythingApplicationTests.java
@@ -4,6 +4,7 @@ import com.eoe.mapper.UserLoginMapper;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
