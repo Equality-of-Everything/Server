4552840aec82a0633be616dbb1ba7efd35388d6d
韩胜路 <1470252635@qq.com>
2023-11-28 16:27:50 +0800

    init

--- src/main/java/com/eoe/controller/UserController.java
+++ src/main/java/com/eoe/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.eoe.controller;
 
+import com.eoe.dto.UserLoginDTO;
 import com.eoe.result.Result;
 import com.eoe.vo.UserLoginVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
@@ -17,5 +20,16 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "用户相关接口")
 public class UserController {
 
-    public Result<UserLoginVO>
+    @ApiOperation(value = "用户登录")
+    @PostMapping("/login")
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
+        log.info("用户登录: {}", userLoginDTO);
+        String username = userLoginDTO.getUsername();
+        String password = userLoginDTO.getPassword();
+
+
+
+
+
+    }
 }
---
+++ src/main/java/com/eoe/dto/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package com.eoe.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(description = "用户登录时传递的数据模型")
+public class UserLoginDTO {
+
+    @ApiModelProperty("用户名")
+    private String username;
+
+    @ApiModelProperty("密码")
+    private String password;
+}
