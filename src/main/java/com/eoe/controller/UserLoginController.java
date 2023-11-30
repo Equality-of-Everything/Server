de877434404661eee9c24e8cb768a4f2c0f10ada
HSLComeon <119283502+HSLComeon@users.noreply.github.com>
2023-11-30 11:23:51 +0800

    Update UserLoginController.java
    增加了对用户名是否存在的判断

--- src/main/java/com/eoe/controller/UserLoginController.java
+++ src/main/java/com/eoe/controller/UserLoginController.java
@@ -1,15 +1,13 @@
 package com.eoe.controller;
 
 import com.eoe.entity.UserLogin;
+import com.eoe.result.Code;
 import com.eoe.result.Result;
 import com.eoe.service.UserLoginService;
 import com.eoe.utils.JWTTokenUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -28,7 +26,6 @@ public class UserLoginController {
     @PostMapping("/register")
     @ApiOperation("用户注册")
     public Result register(@RequestBody UserLogin userLogin) {
-        System.out.println("register....");
         boolean flag = userLoginService.register(userLogin);
         if(flag) return new Result(flag, "注册成功", null);
         return new Result(flag, "注册失败,用户名重复", null);
@@ -38,11 +35,15 @@ public class UserLoginController {
     @PostMapping("/login")
     @ApiOperation("用户登录")
     public Result login(@RequestBody UserLogin userLogin) {
-        boolean flag = userLoginService.login(userLogin);
-        if(!flag) return new Result(flag, "登录失败，密码或用户名错误", null);
+        boolean flaglogin = userLoginService.login(userLogin);
+        boolean flagcheckUserName = userLoginService.checkUsername(userLogin.getUsername());
+        if(!flagcheckUserName) return new Result(flagcheckUserName, "用户名不存在", null, Code.LOGIN_ERROR_NOUSER);
+        if(!flaglogin) return new Result(flaglogin, "登录失败", null, Code.LOGIN_ERROR_PASSWORD);
         String token = JWTTokenUtil.generateToken(userLogin);
         return new Result(true, "登录成功", token);
     }
+
+
 
 
 
